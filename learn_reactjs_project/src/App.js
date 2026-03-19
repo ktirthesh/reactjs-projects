697c9fa7c03bf2ea9d35824eb4e3790882603d11
ktirthesh <tirthesh.karande@gmail.com>
2026-03-19 19:57:04 +0530

    app.js changes applied

--- learn_reactjs_project/src/App.js
+++ learn_reactjs_project/src/App.js
@@ -1,7 +1,14 @@
-import UseReducerExample from "./Hooks/UseReducerExample";
+// import UseActionStateExample from "./Hooks/UseActionStateExample";
+// import UseReducerExample from "./Hooks/UseReducerExample";
+
+import { Checkout } from "./Hooks/UseActionStateExample";
+import CounterCallBackExample from "./Hooks/UseCallBackExample";
 
 function App() {
-  return <UseReducerExample />;
+  // return <UseReducerExample />;
+  // return <UseActionStateExample />;
+  // return <CounterCallBackExample />;
+  return <Checkout />;
 }
 
 export default App;
